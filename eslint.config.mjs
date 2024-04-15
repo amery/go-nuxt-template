bc9bb5d38ea72e5c68025dcb99e3521bb6761bb8
Alejandro Mery <amery@geeks.cl>
2024-04-15 18:36:36 +0000

    eslint: disable vue/multi-word-component-names
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ export default withNuxt({
     '**/*.{cjs,mjs,js,ts,vue}',
   ],
   rules: {
+    'vue/multi-word-component-names': "off",
     '@stylistic/brace-style': ['error', '1tbs'],
     '@stylistic/indent': ['error', 2],
     '@stylistic/quotes': ['error', 'single'],
