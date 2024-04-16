8f7f04fd84ae3c5786aaece4063fa04fa89f8a61
Alejandro Mery <amery@geeks.cl>
2024-04-16 21:43:18 +0000

    web/client: add /grid-in-card page showing the problem keeping a grid contained
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ web/client/src/components/card-with-grid.vue
@@ -0,0 +1,29 @@
+<template>
+  <UDashboardCard
+    :title="title || 'Card'"
+    class="shadow-lg"
+  >
+    <div class="flex-1 border overflow-x-scroll">
+      <ul class="grid grid-rows-10 grid-flow-col">
+        <li
+          v-for="i in count"
+          :key="i"
+        >
+          <div class="flex flex-0 w-32 board m-1 p-2 bg-primary">
+            {{ i }}
+          </div>
+        </li>
+      </ul>
+    </div>
+  </UDashboardCard>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+  count: {
+    type: Number,
+    default: 100,
+  },
+});
+</script>
---
+++ web/client/src/pages/grid-in-card.vue
@@ -0,0 +1,26 @@
+<template>
+  <UDashboardPanel grow>
+    <UDashboardPanelContent>
+      <card-with-grid
+        class="h-[50%]"
+        title="Card 1"
+        :count="200"
+      />
+      <card-with-grid
+        class="h-[50%] mt-4"
+        title="Card 2"
+        :count="200"
+      />
+      <div class="flex-1" />
+      <div class="pt-2 px-4">
+        Footer
+      </div>
+    </UDashboardPanelContent>
+  </UDashboardPanel>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard',
+});
+</script>
