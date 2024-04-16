2640561821ede9b06815ec453d7636f5728c2947
Alejandro Mery <amery@geeks.cl>
2024-04-16 21:42:57 +0000

    web/client: initial dashboard layout
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ web/client/src/layouts/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <UDashboardLayout>
+    <UDashboardPanel
+      :width="250"
+      collapsible
+    >
+      <UDashboardNavbar>
+        <template #left>
+          Logo
+        </template>
+      </UDashboardNavbar>
+      <UDashboardSidebar>
+        Sidebar
+        <UDivider class="sticky bottom-0" />
+
+        <template #footer>
+          <span class="text-sm">
+            Sidebar Footer
+          </span>
+          <UColorModeToggle class="right-0" />
+        </template>
+      </UDashboardSidebar>
+    </UDashboardPanel>
+
+    <UDashboardPage>
+      <NuxtPage />
+    </UDashboardPage>
+  </UDashboardLayout>
+</template>
